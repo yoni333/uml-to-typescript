9f43c2bcbf70e237039d32fa31510038e0b324d1
yehonatan yehezkel <yoni333@gmail.com>
2022-01-04 18:24:24 +0200

    inhert

--- src/app/inhert.component.ts
+++ src/app/inhert.component.ts
@@ -1,22 +1,32 @@
-import { Component, Input } from '@angular/core';
-import { EInhert, InhertUml } from './class.type';
+import { Component, Input, OnChanges } from "@angular/core";
+import { EInhert, InhertUml } from "./class.type";
 
 @Component({
-  selector: 'inhert',
-  template: ` 
-    <span *ngIf='inhert.type===EInhert.None ; else inhert'
-        (click)="editInhert= !editInhert" 
-        class="open"
-        >&#123; + &#125;</span>
-    <span ></span>
-    <span #inhert>inhert</span>
+  selector: "inhert",
+  template: `
+    {{ inhert.type }}
+    <ng-container *ngIf="inhert.type === EInhert.None; else inhert">
+      <span (click)="editInhert = !editInhert" class="open"
+        >&#123; + &#125;</span
+      >
+    </ng-container>
 
+    <ng-template #inhert>
+      {{ inhert }}
+      <span>{{
+        inhert.type === EInhert.Implements ? "Implements" : "Extend"
+      }}</span>
+    </ng-template>
   `,
-  styles: [],
+  styles: []
 })
-export class InhertComponent {
+export class InhertComponent implements OnChanges {
   EInhert = EInhert;
   @Input() inhert: InhertUml;
   show: boolean = true;
   editInhert: boolean = false;
+
+  ngOnChanges(e) {
+    console.log(e);
+  }
 }
